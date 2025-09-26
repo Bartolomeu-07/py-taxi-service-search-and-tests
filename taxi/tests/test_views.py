3de7865e167f17f8a10b419c6ba923db3b2832c6
Bartolomeu <bartoszokroj@wp.pl>
2025-09-26 22:28:20 +0200

    solution

--- taxi/tests/test_views.py
+++ taxi/tests/test_views.py
@@ -59,22 +59,20 @@ class TestViews(TestCase):
         response = self.client.get(
             reverse("taxi:manufacturer-list"), {"name": "e"}
         )
-        self.assertEqual(
-            list(response.context["manufacturer_list"]), []
-        )
+        self.assertEqual(list(response.context["manufacturer_list"]), [])
 
-        response = self.client.get(
-            reverse("taxi:manufacturer-list")
-        )
+        response = self.client.get(reverse("taxi:manufacturer-list"))
         self.assertEqual(
-            list(response.context["manufacturer_list"]), list(Manufacturer.objects.all())
+            list(response.context["manufacturer_list"]),
+            list(Manufacturer.objects.all()),
         )
 
         response = self.client.get(
             reverse("taxi:manufacturer-list"), {"name": "w"}
         )
         self.assertEqual(
-            list(response.context["manufacturer_list"]), [manufacturer1, manufacturer2]
+            list(response.context["manufacturer_list"]),
+            [manufacturer1, manufacturer2],
         )
 
     def test_search_driver_by_username(self):
@@ -82,39 +80,31 @@ class TestViews(TestCase):
             username="moving",
             first_name="Admin",
             last_name="Admin",
-            license_number="CDE876234"
+            license_number="CDE876234",
         )
         admin2 = get_user_model().objects.create_superuser(
             username="stag",
             first_name="Admin",
             last_name="Admin",
-            license_number="BIE00238"
+            license_number="BIE00238",
         )
 
         response = self.client.get(
             reverse("taxi:driver-list"), {"username": "v"}
         )
-        self.assertEqual(
-            list(response.context["driver_list"]), [admin1]
-        )
+        self.assertEqual(list(response.context["driver_list"]), [admin1])
 
         response = self.client.get(
             reverse("taxi:driver-list"), {"username": "s"}
         )
-        self.assertEqual(
-            list(response.context["driver_list"]), [admin2]
-        )
+        self.assertEqual(list(response.context["driver_list"]), [admin2])
 
         response = self.client.get(
             reverse("taxi:driver-list"), {"username": "z"}
         )
-        self.assertEqual(
-            list(response.context["driver_list"]), []
-        )
+        self.assertEqual(list(response.context["driver_list"]), [])
 
-        response = self.client.get(
-            reverse("taxi:driver-list")
-        )
+        response = self.client.get(reverse("taxi:driver-list"))
         self.assertEqual(
             list(response.context["driver_list"]), list(Driver.objects.all())
         )
@@ -141,38 +131,20 @@ class TestViews(TestCase):
             model="M3",
         )
 
-        response = self.client.get(
-            reverse("taxi:car-list"), {"model": "9"}
-        )
-        self.assertEqual(
-            list(response.context["car_list"]), [car1]
-        )
+        response = self.client.get(reverse("taxi:car-list"), {"model": "9"})
+        self.assertEqual(list(response.context["car_list"]), [car1])
 
-        response = self.client.get(
-            reverse("taxi:car-list"), {"model": "m"}
-        )
-        self.assertEqual(
-            list(response.context["car_list"]), [car2]
-        )
+        response = self.client.get(reverse("taxi:car-list"), {"model": "m"})
+        self.assertEqual(list(response.context["car_list"]), [car2])
 
-        response = self.client.get(
-            reverse("taxi:car-list"), {"model": "0"}
-        )
-        self.assertEqual(
-            list(response.context["car_list"]), []
-        )
+        response = self.client.get(reverse("taxi:car-list"), {"model": "0"})
+        self.assertEqual(list(response.context["car_list"]), [])
 
-        response = self.client.get(
-            reverse("taxi:car-list")
-        )
+        response = self.client.get(reverse("taxi:car-list"))
         self.assertEqual(
             list(response.context["car_list"]), list(Car.objects.all())
         )
 
-        response = self.client.get(
-            reverse("taxi:car-list"), {"model": "3"}
-        )
-        self.assertEqual(
-            list(response.context["car_list"]), [car1, car2]
-        )
+        response = self.client.get(reverse("taxi:car-list"), {"model": "3"})
+        self.assertEqual(list(response.context["car_list"]), [car1, car2])
         self.assertTemplateUsed(response, "taxi/car_list.html")
